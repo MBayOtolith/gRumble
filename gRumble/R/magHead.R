34c3898c5068565186974ed7f4b34cbc66caa1f0
Connor White <connor.white@gmail.com>
2017-05-16 12:25:11 -0700

    magHead fix

--- gRumble/R/magHead.R
+++ gRumble/R/magHead.R
@@ -1,7 +1,7 @@
 magHead<-function(PR,magxyz,magOff){
-  magxyz[,1]<- -magOff[1]
-  magxyz[,2]<- -magOff[2]
-  magxyz[,3]<- -magOff[3]
+  magxyz[,1]<- magxyz[,1]-magOff[1]
+  magxyz[,2]<- magxyz[,2]-magOff[2]
+  magxyz[,3]<- magxyz[,3]-magOff[3]
   yH<-magxyz[,3]*sin(PR[,2]) - magxyz[,2]*cos(PR[,2])
   xH<-magxyz[,1]*cos(PR[,1]) + magxyz[,2]*sin(PR[,1])*sin(PR[,2])+ magxyz[,3]*sin(PR[,1])*cos(PR[,2])
   Heading<-atan2(-yH,xH)
