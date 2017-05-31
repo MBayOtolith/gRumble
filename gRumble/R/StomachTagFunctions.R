7f9f97453f2a9d0ef90a50af17a0f3550e64b884
jhMox <jmoxley@mbayaq.org>
2017-05-31 10:37:54 -0700

    bug fix on indexing for vv1 in EstAngles

--- gRumble/R/StomachTagFunctions.R
+++ gRumble/R/StomachTagFunctions.R
@@ -34,7 +34,7 @@ ZXYb<-function(Xr,Yr,Zr){
 }
 
 
-xyzTrans<-function(xyz,Xr=0,Yr=0,Zr=0){ 
+xyzTrans<-function(xyz,Xr=0,Yr=0,Zr=0){
   xyz<-xyz %*% XYZb(Xr,Yr,Zr)
   return(xyz)
 }
@@ -52,14 +52,14 @@ XYrotAng<-function(xyz){
   pitch = atan2((-pos[1]),((pos[2] * sin(roll)) + (pos[3]*cos(roll))))
   Xr=roll
   Yr=pi-pitch
-  
+
   return(c(Xr,Yr))
 }
 
 
 
 
-ZrotAng<-function(xyz,dat_Freq=5, signal=c(.3,.7),wind=7, by=pi/360, head=0,lim=c(-pi/2,pi/2),weighting=FALSE){ 
+ZrotAng<-function(xyz,dat_Freq=5, signal=c(.3,.7),wind=7, by=pi/360, head=0,lim=c(-pi/2,pi/2),weighting=FALSE){
   if(any(is.na(xyz))){
     stop("NAs in data")
   }
@@ -72,17 +72,17 @@ ZrotAng<-function(xyz,dat_Freq=5, signal=c(.3,.7),wind=7, by=pi/360, head=0,lim=
   ###Output frequencies for the FFT
   fs<-(0:((nrow(xyz)/2) - 1)) * dat_Freq/nrow(xyz)
   locs<-which(fs>signal[1] & fs<signal[2])
-  
+
   mags<-NULL
   for(x in 1:length(zrot)){
     xyz_cor<-Zb(zrot[x])%*%t(xyz)
     xyz_cor<-t(xyz_cor)
-    
+
     filt<-filter(xyz_cor[,2],filter=rep(1,dat_Freq*wind)/(dat_Freq*wind), sides = 2,circular = TRUE)
     dyn<-xyz_cor[,2]-filt
     y.fft<-Mod(fft(dyn))
     mags<-cbind(mags,y.fft[locs])
-    
+
   }
   theta<-zrot[which(mags== max(mags),arr.ind=TRUE)[2]]
   if(weighting==TRUE){
@@ -97,27 +97,27 @@ ZrotAng<-function(xyz,dat_Freq=5, signal=c(.3,.7),wind=7, by=pi/360, head=0,lim=
 
 findBreaks<-function(VV = NULL,corrFreq= 30,minsize=NULL,lims=c(-.05,.05), dat_Freq=5){
   size<-30 #Freq
-  
+
   if(is.null(minsize)){
     minsize <- CorrFreq
   }
-  
+
   exp<-floor((minsize- corrFreq) /2)* dat_Freq
-  
+
   loc<-seq(1,length(VV), by = corrFreq*dat_Freq)
   Breaks<-matrix(0,nrow=(length(loc)-1),ncol=4)
-  
+
   for(i in 1:(length(loc)-1)){
     start<-loc[i] - exp
     end<-loc[i+1] + exp
-    
+
     if(start<1){
       start<-1
     }
     if(end>length(VV)){
       end<-length(VV)
     }
-    
+
     d<-mean(VV[start:end])
     while((d > lims[2]) | (d < lims[1])){
       start<-start-5
@@ -146,33 +146,33 @@ findBreaks<-function(VV = NULL,corrFreq= 30,minsize=NULL,lims=c(-.05,.05), dat_F
 
 EstAngles<-function(dat,vv, breaks,dat_Freq = 5,signal = c(.3,.7),wind = 7,Zby = pi/180, lim=c(-pi/2,pi/2),weighting=FALSE){
   est_angles<-matrix(0,nrow=nrow(breaks),ncol=7)
-  
+
   #'Initialize' the model to calculate a heading
   dtemp<-dat[breaks[1,2]:breaks[1,3],]
-  vv1<-vv[breaks[1,3]:breaks[1,3]]
+  vv1<-vv[breaks[1,2]:breaks[1,3]]
   dtemp<-dtemp[complete.cases(dtemp[,1:3]),]
   vv1<-vv1[complete.cases(dtemp[,1:3])]
-  
+
   XYr<-XYrotAng(dtemp[,1:3])
   Zr<-ZrotAng(xyzTrans(as.matrix(dtemp[,1:3]),Xr=XYr[1],Yr=XYr[2],Zr=0),
               dat_Freq = dat_Freq,signal = signal,wind = wind,by = Zby,head=0,lim=c(0,pi),weighting=weighting)
-  
+
   for(i in 1:nrow(breaks)){
-    
+
     dtemp<-dat[breaks[i,2]:breaks[i,3],]
     vv1<-vv[breaks[i,2]:breaks[i,3]]
     dtemp<-dtemp[complete.cases(dtemp[,1:3]),]
     vv1<-vv1[complete.cases(dtemp[,1:3])]
-    
+
     XYr<-XYrotAng(dtemp[,1:3])
     Zr<-ZrotAng(xyzTrans(as.matrix(dtemp[,1:3]),Xr=XYr[1],Yr=XYr[2],Zr=0),
                 dat_Freq = dat_Freq,signal = signal,wind = wind,by = Zby,head=Zr,lim=lim,weighting=weighting)
-    
+
     xyz<-xyzTrans(as.matrix(dtemp[,1:3]),Xr=XYr[1],Yr=XYr[2], Zr=Zr)
-    
+
     p<-filter(xyz[,1],filter = rep(1,50)/(50), sides = 2,circular = TRUE)
     m<-lm(p ~ vv1)
-    
+
     est_angles[i,1]<-breaks[i,1]
     est_angles[i,2]<-breaks[i,2]
     est_angles[i,3]<-breaks[i,3]
@@ -196,10 +196,10 @@ angleGapInterp<-function(dat,events){
     if(d > pi| d < -pi){
       ang1<-dat[loc[i,1]-1]- (pi/2)
       ang2<-dat[loc[i,2]+1]- (pi/2)
-      
+
       angmag<-pi-abs(ang1) + pi-abs(ang2)
       angs<-seq(0,angmag,length.out=leng[i]+1)
-      
+
       if(ang1 < ang2){
         angs<-ang1+angs*-1
         switc<-which(angs < -pi)
@@ -228,21 +228,21 @@ angInterp<-function(estAngles, data){
   Xr[1]<-estAngles[1,4]
   Xr[nrow(data)]<-estAngles[nrow(estAngles),4]
   Xr[estAngles[,1]] <- estAngles[,4]
-  
+
   Yr<-rep(NA,nrow(data))
   Yr[1]<-estAngles[1,5]
   Yr[nrow(data)]<-estAngles[nrow(estAngles),5]
   Yr[estAngles[,1]] <- estAngles[,5]
-  
+
   Zr<-rep(NA,nrow(data))
   Zr[1]<-estAngles[1,6]
   Zr[nrow(data)]<-estAngles[nrow(estAngles),6]
   Zr[estAngles[,1]] <- estAngles[,6]
-  
+
   Xr<-angleGapInterp(Xr,is.na(Xr))
   Yr<-angleGapInterp(Yr,is.na(Yr))
   Zr <-eventInterp(Zr,is.na(Zr))
-    
+
   return(cbind(Xr,Yr,Zr))
 }
 
@@ -289,13 +289,13 @@ Gsep<-function(xyz,filt=rep(1,5*5)/(5*5)){
   X_Static<-filter(xyz[,1],filter = filt, sides = 2,circular = TRUE)
   Y_Static<-filter(xyz[,2],filter = filt, sides = 2,circular = TRUE)
   Z_Static<-filter(xyz[,3],filter = filt, sides = 2,circular = TRUE)
-  
+
   X_Dynamic<-xyz[,1] - X_Static
   Y_Dynamic<-xyz[,2] - Y_Static
   Z_Dynamic<-xyz[,3] - Z_Static
-  
+
   ODBA<- (abs(X_Dynamic) + abs(Y_Dynamic) + abs(Z_Dynamic))
-  
+
   return(cbind(X_Static,Y_Static,Z_Static,X_Dynamic,Y_Dynamic,Z_Dynamic,ODBA))
 }
 
@@ -304,15 +304,15 @@ Gsep<-function(xyz,filt=rep(1,5*5)/(5*5)){
 tag<-function(x, y ,ang=0,exp=1,col="orange"){
 
   xlocs<-c(-0.25,-.25,.25,.25)
-  
+
   ylocs<- c(-.25,.25,.25,-.25)
-  
+
   shapePlot(x=x, y=y, shapeX=xlocs, shapeY=ylocs, ang=ang, exp=exp, col=col)
 }
 
 
 sharkback<-function(x, y, ang=0,exp=1,col="lightblue4"){
-  
+
   ylocs<-c(1.002,  0.988,  0.920,  0.857,  0.752,  0.643,  0.594,  0.494,
            0.462, 0.430, 0.371, 0.321, 0.258, 0.172, 0.117, 0.131, 0.172, 0.185,
             0.199,  0.117,  0.018, -0.159, -0.282, -0.363,
@@ -321,7 +321,7 @@ sharkback<-function(x, y, ang=0,exp=1,col="lightblue4"){
            -0.540, -0.445, -0.363, -0.282, -0.159,  0.018,  0.117,  0.199,
            0.185, 0.172, 0.131, 0.117, 0.172, 0.258, 0.321, 0.371, 0.430, 0.462,
            0.494, 0.594,  0.643,  0.752,  0.857,  0.920, 0.988,  1.002)
-  
+
   xlocs<-c(-0.002, -0.025, -0.051, -0.086, -0.103, -0.128, -0.135, -0.135,
            -0.133, -0.171, -0.258, -0.326, -0.378, -0.397, -0.371, -0.303, -0.203, -0.138,
            -0.132, -0.132, -0.128, -0.122, -0.116, -0.112,
@@ -331,24 +331,24 @@ sharkback<-function(x, y, ang=0,exp=1,col="lightblue4"){
            0.138,  0.203,  0.303,  0.371,  0.397,  0.378, 0.326,  0.258, 0.171,  0.133,
            0.135,  0.135,  0.128,  0.103,  0.086,  0.051, 0.025,  0.002)
   shapePlot(x=x, y=y, shapeX=xlocs, shapeY=ylocs, ang=ang, exp=exp, col=col)
-  
-  
+
+
   ylocs<-c(0.24,  0.122,  0.058, -0.06, -0.06,  0.058,  0.122,  0.24)
   xlocs<-c(-0.005, -0.031, -0.027, -0.004,  0.004,  0.027,  0.031,  0.005)
-  
+
   shapePlot(x=x, y=y, shapeX=xlocs, shapeY=ylocs, ang=ang, exp=exp, col=col)
 }
 
 
-shark<-function(x, y, ang=0,exp=1,col="lightblue4"){  
+shark<-function(x, y, ang=0,exp=1,col="lightblue4"){
   xlocs<-c(-0.540, -0.695, -0.705, -0.645, -0.750, -0.745, -0.535,
            -0.075, -0.105, -0.100, 0.075, 0.295, 0.470, 0.575,
            0.605, 0.615, 0.535, 0.345, 0.015, -0.175)
-  
+
   ylocs<- c(-0.030, -0.270, -0.225, -0.005, 0.315, 0.385, 0.030,
             0.125, 0.245, 0.275, 0.130, 0.115, 0.090, 0.040, 0.005,
             -0.030, -0.070, -0.110, -0.125, -0.105)
-  
+
   shapePlot(x=x, y=y, shapeX=xlocs, shapeY=ylocs, ang=ang, exp=exp, col=col)
 }
 
@@ -363,7 +363,7 @@ sharkhead<-function(x,y,ang=0,exp=1,col="lightblue4"){
            -0.07753, -0.11834, -0.16822, -0.23624, -0.33,
            -0.26344, -0.22263, -0.18636, -0.17276, -0.23170,
            -0.26344, -0.27705)
-  
+
   xlocs<-c(0.001213, -0.12418, -0.269817, -0.346677, -0.423537,
            -0.528713, -0.625799, -0.75, -0.641980, -0.544894,
            -0.431627, -0.358813, -0.338587, -0.285998, -0.217229,
@@ -372,7 +372,7 @@ sharkhead<-function(x,y,ang=0,exp=1,col="lightblue4"){
            0.087781,  0.124188,  0.217229,  0.285998,  0.338587,
            0.358813,  0.431627,  0.544894,  0.641980,  0.75,
            0.625799,  0.528713,  0.423537,  0.346677,  0.269817,
-           0.124188, -0.001213)  
+           0.124188, -0.001213)
   shapePlot(x=x, y=y, shapeX=xlocs, shapeY=ylocs, ang=ang, exp=exp, col=col)
 }
 
@@ -381,18 +381,18 @@ shapePlot<-function(x, y, shapeX=NULL,shapeY=NULL,ang=0,exp=1,col="lightblue4"){
   op<-par()
   xdif<-op$usr[2]-par()$usr[1]
   ydif<-op$usr[4]-par()$usr[3]
-  
+
   windasp<-op$pin[1]/op$pin[2]
-  
-  
+
+
   ang<-(ang/180)*pi
   xy<-Zb(ang) %*% rbind(shapeX,shapeY,rep(0,length(x)))
-  
+
   xlocs<-(xy[1,]*exp*(xdif/2)) + x
   ylocs<-(xy[2,]*exp*windasp*(ydif/2)) + y
-  
-  polygon(x=xlocs,y=ylocs, col=col)  
-  
+
+  polygon(x=xlocs,y=ylocs, col=col)
+
 }
 
 
